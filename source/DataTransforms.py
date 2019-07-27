3e212e481d987984ab87696406fc8c5f9e5bd6e5
Perledition <maxim.perl@outlook.de>
2019-07-27 16:39:11 +0200

    transform api

--- source/DataTransforms.py
+++ source/DataTransforms.py
@@ -1,37 +1,29 @@
 # DataService
 # import pandas as pd
-from flask import Flask
+import json
+from flask import Flask, jsonify, request, Response
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-api = Api(app)
+CORS(app)
+# api = Api(app)
 
 
-class ColumnFilter(Resource):
-
-    def get(self):
-        return {
-            'product': [
-                'ice',
-                'water',
-                'Butter',
-                'schwanzringel',
-            ]
-        }
-
-    def post(self, request):
-        return {
-            'product': [
-                'ice',
-                'water',
-                'Butter',
-                str(request.POST['Type']),
-            ]
-        }
+@app.route('/transform', methods=['GET', 'POST'])
+def transform():
+    data = {'Product': [1, 2, 4]}
+    if request.method == 'GET':
+        return jsonify(data)
+    elif request.method == 'POST':
+        # content = request.json['Type']
+        resp = Response(request, status=200, mimetype='application/json')
+        return resp
+    else:
+        return jsonify({'Error': 'Method not allowed'})
 
 
-api.add_resource(ColumnFilter, '/')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
